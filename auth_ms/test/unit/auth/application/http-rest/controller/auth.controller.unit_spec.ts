616d78fdf955293fd776f45c4ec92db47b23d48d
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2022-07-05 16:24:59 -0500

    <<AuthController, Unit Test, Red>>

--- auth_ms/test/unit/auth/application/http-rest/controller/auth.controller.unit_spec.ts
+++ auth_ms/test/unit/auth/application/http-rest/controller/auth.controller.unit_spec.ts
@@ -4,10 +4,13 @@ import SignUpServiceMock from '@test/unit/utils/mocks/sign_up.service.mock';
 import SignUpInteractor from '@core/domain/auth/use-case/interactor/sign_up.interactor';
 import { sign_up_credentials_stub } from '@test/unit/utils/stubs/sign_up_credentials.stub';
 import AuthDITokens from '@core/domain/auth/di/auth_di_tokens';
+import ValidateCredentialsInteractor from '@core/domain/auth/use-case/interactor/validate_credentials.interactor';
 
 describe('AuthController', () => {
   let auth_controller: AuthController;
   let sign_up_interactor: SignUpInteractor;
+  let validate_credentials_interactor: ValidateCredentialsInteractor;
+  let validate_credentials_output: unknown;
 
   beforeEach(async () => {
     const module_ref = await Test.createTestingModule({
@@ -22,6 +25,7 @@ describe('AuthController', () => {
 
     auth_controller = module_ref.get<AuthController>(AuthController);
     sign_up_interactor = module_ref.get<SignUpInteractor>(AuthDITokens.SignUpInteractor);
+    validate_credentials_interactor = module_ref.get<ValidateCredentialsInteractor>(AuthDITokens.ValidateCredentialsInteractor);
 
     jest.clearAllMocks();
   });
@@ -34,6 +38,22 @@ describe('AuthController', () => {
 
       it('should call sign_up_interactor', () => {
         expect(sign_up_interactor.execute).toBeCalledWith(sign_up_credentials_stub());
+      });
+    });
+  });
+
+  describe('validate login credentials', () => {
+    describe('when validateCredentials is called', () => {
+      beforeEach(async () => {
+        validate_credentials_output = await auth_controller.validateCredentials(sign_up_credentials_stub())
+      });
+
+      it('should call validate_credentials_interactor', () => {
+        expect(validate_credentials_interactor.execute).toBeCalledWith(sign_up_credentials_stub());
+      });
+
+      it('should return a response with the property "are_credentials_valid"', () => {
+        expect(validate_credentials_output).toHaveProperty('are_credentials_valid');
       });
     });
   });
